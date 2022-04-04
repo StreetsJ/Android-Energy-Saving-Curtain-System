86566632ed3635653683a3bedaf950cc46d5c6bd
James Streets <james.m.streets@gmail.com>
2022-04-03 21:28:05 -0500

    Basic UI and colors xml as well as button open/close & auto/man logic

--- Andriod-Dev/app/src/main/java/tamu/edu/smartcurtain/MainActivity.java
+++ Andriod-Dev/app/src/main/java/tamu/edu/smartcurtain/MainActivity.java
@@ -7,8 +7,9 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    Button open_btn, close_btn, man_btn, auto_btn;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button open_btn, close_btn, man_btn, auto_btn;
+    private int prim_color, open_color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,37 +21,53 @@ public class MainActivity extends AppCompatActivity {
         close_btn = (Button) findViewById(R.id.close_btn);
         man_btn = (Button) findViewById(R.id.man_btn);
         auto_btn = (Button) findViewById(R.id.auto_btn);
+
+        // Override the onclick listener for our own functions below
+        open_btn.setOnClickListener(this);
+        close_btn.setOnClickListener(this);
+        man_btn.setOnClickListener(this);
+        auto_btn.setOnClickListener(this);
+
+        // get colors from xml
+        prim_color = getResources().getColor(R.color.light_blue);
+        open_color = getResources().getColor(R.color.light_green);
+
+        man_btn.setBackgroundColor(open_color);
     }
 
-    public void onOpenClick(View view) {
-        open_btn.setBackgroundColor(Color.GREEN);
-        close_btn.setBackgroundColor(getResources().getColor(R.color.purple_500));
-    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.open_btn:
+                open_btn.setBackgroundColor(open_color);
+                close_btn.setBackgroundColor(prim_color);
+                break;
+            case R.id.close_btn:
+                open_btn.setBackgroundColor(prim_color);
+                close_btn.setBackgroundColor(open_color);
+                break;
+            case R.id.man_btn:
+                open_btn.setBackgroundColor(prim_color);
+                close_btn.setBackgroundColor(prim_color);
 
-    public void onCloseClick(View view) {
-        open_btn.setBackgroundColor(getResources().getColor(R.color.purple_500));
-        close_btn.setBackgroundColor(Color.GREEN);
-    }
+                man_btn.setBackgroundColor(Color.MAGENTA);
+                auto_btn.setBackgroundColor(prim_color);
 
-    public void onManualClick(View view) {
-        open_btn.setBackgroundColor(getResources().getColor(R.color.purple_500));
-        close_btn.setBackgroundColor(getResources().getColor(R.color.purple_500));
+                open_btn.setEnabled(true);
+                close_btn.setEnabled(true);
+                break;
+            case R.id.auto_btn:
+                open_btn.setBackgroundColor(prim_color);
+                close_btn.setBackgroundColor(prim_color);
 
-        man_btn.setBackgroundColor(Color.MAGENTA);
-        auto_btn.setBackgroundColor(getResources().getColor(R.color.purple_500));
+                auto_btn.setBackgroundColor(Color.MAGENTA);
+                man_btn.setBackgroundColor(prim_color);
 
-        open_btn.setEnabled(true);
-        close_btn.setEnabled(true);
-    }
-
-    public void onAutoClick(View view) {
-        open_btn.setBackgroundColor(getResources().getColor(R.color.purple_500));
-        close_btn.setBackgroundColor(getResources().getColor(R.color.purple_500));
-
-        auto_btn.setBackgroundColor(Color.MAGENTA);
-        man_btn.setBackgroundColor(getResources().getColor(R.color.purple_500));
-
-        open_btn.setEnabled(false);
-        close_btn.setEnabled(false);
+                open_btn.setEnabled(false);
+                close_btn.setEnabled(false);
+                break;
+            default:
+                break;
+        }
     }
 }
